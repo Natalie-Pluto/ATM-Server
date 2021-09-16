cc528f8e4ce7b2944709f1bcbf2d14c91a50a395
Ali <alie0302@uni.sydney.edu.au>
2021-09-16 17:48:24 +1000

    added sql_update method to DB object class and filled in block and unblock methods

--- app/src/main/java/ATMSystem/DB.java
+++ app/src/main/java/ATMSystem/DB.java
@@ -2,19 +2,42 @@ package ATMSystem;
 import java.sql.*;
 
 public class DB {
+	static final String DB_URL = "localhost";
+	static final String USERNAME = "alien";
+	static final String PASSWORD = "fuck"; 
+
 	private String cardNumber; // If cardNumber is null, it means no card is authenticated.	
 	
-	private String query() {
-		//TODO 
+	private boolean sql_update(String query) {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(query); 
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	private String sql_select() {
 		return null;
 	}
 	
 	public String getCardNumber() {return this.cardNumber;}
 	public void confiscate() {} //TODO
-	public void block() {} //TODO
+	public boolean block() {
+		return sql_update("UPDATE atmserver.\"Card\" SET blocked = true WHERE card_number = '" + cardNumber + ";");
+	} 
+	public boolean unblock() {
+		return sql_update("UPDATE atmserver.\"Card\" SET blocked = true WHERE card_number = '" + cardNumber + ";");
+	}
 	public void subtractBalance(double x) {} //TODO
 	public void addBalance(double x) {} //TODO
 	public double getBalance() {return 0;} //TODO
 	public boolean authenticate(String cardNumber, String pin) {return false;} //TODO
+
 		
 }
+ 
