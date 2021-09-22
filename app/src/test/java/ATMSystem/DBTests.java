2dad80ae1386e7574feb12f67df8f7d15561e5ce
Ali <alie0302@uni.sydney.edu.au>
2021-09-22 14:32:00 +1000

    added DBTests class (hardly anything in it though) and added list of test ideas for DB object

---
+++ app/src/test/java/ATMSystem/DBTests.java
@@ -0,0 +1,16 @@
+package ATMSystem;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DBTests {
+    @Test
+    public void initOk() {
+        //todo
+    }
+
+    @Test
+    public void authenticateFailure() {
+        //todo
+    }
+}
