bf89af7890834a8a32baad3e57e498d9d87645d5
Ali <alie0302@uni.sydney.edu.au>
2021-09-22 15:13:39 +1000

    added jacoco and mockito stuff to build.gradle and added one test for DB

--- app/src/test/java/ATMSystem/DBTests.java
+++ app/src/test/java/ATMSystem/DBTests.java
@@ -7,10 +7,12 @@ public class DBTests {
     @Test
     public void initOk() {
         //todo
+        DB x = new DB("11111", "a_username", "a_password","a_url");
     }
 
     @Test
     public void authenticateFailure() {
         //todo
+
     }
 }
